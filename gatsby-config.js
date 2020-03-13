ff62e731e392590aab4d2c8f41ef14348ce1789e
Jason <bilafish@gmail.com>
2020-03-13 16:45:59 +0800

    :mag: Update site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jason Zheng - Front-End Developer`,
+    description: `Personal portfolio site showcasing my works`,
+    author: `Jason Zheng`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
