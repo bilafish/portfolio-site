5b8c11c787b0250829a101e3c7b68660eda371a4
Jason <bilafish@gmail.com>
2020-03-13 18:29:21 +0800

    :package: Install styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,13 +18,19 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Jason Zheng - Front-End Web Developer`,
+        short_name: `Jason - Web Developer`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
