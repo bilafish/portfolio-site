4f771a9c3ab1787750e1ff65b7ab951f739beef6
Jason <bilafish@gmail.com>
2020-03-13 20:53:38 +0800

    :truck: Renamed page-2 to works

--- src/pages/works.js
+++ src/pages/works.js
@@ -6,9 +6,8 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <SEO title="Works" />
+    <h1>🚧 Under Construction</h1>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
