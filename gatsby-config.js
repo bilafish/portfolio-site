ebd325304539c4ff352e6fddddb9295c21d7ca25
Jason <bilafish@gmail.com>
2020-03-16 14:15:56 +0800

    :package: Installed Gatsby plugin for svgs

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,14 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /images/, // See below to configure properly
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Jason Zheng - Front-End Web Developer`,
